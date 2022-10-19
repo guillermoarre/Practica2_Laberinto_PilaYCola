c556e7107c7ca0dd0a5d516ebb071ef8909ee2e2
guillermoarre <guillermo.arredondo247@gmail.com>
2022-10-19 10:44:57 -0500

    Se agrega el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Practica2_Laberinto_PilaYCola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbcola = new System.Windows.Forms.ListBox();
+            this.lbpila = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(12, 12);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(551, 542);
+            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pb.TabIndex = 0;
+            this.pb.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(612, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(759, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Resolver";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbcola
+            // 
+            this.lbcola.FormattingEnabled = true;
+            this.lbcola.HorizontalScrollbar = true;
+            this.lbcola.Location = new System.Drawing.Point(569, 62);
+            this.lbcola.Name = "lbcola";
+            this.lbcola.Size = new System.Drawing.Size(337, 186);
+            this.lbcola.TabIndex = 3;
+            // 
+            // lbpila
+            // 
+            this.lbpila.FormattingEnabled = true;
+            this.lbpila.HorizontalScrollbar = true;
+            this.lbpila.Location = new System.Drawing.Point(569, 303);
+            this.lbpila.Name = "lbpila";
+            this.lbpila.Size = new System.Drawing.Size(337, 238);
+            this.lbpila.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(692, 568);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "SALIR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(569, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "COLA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(569, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PILA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 603);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lbpila);
+            this.Controls.Add(this.lbcola);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pb);
+            this.Name = "Form1";
+            this.Text = "Práctica 2, LABERINTO";
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox lbcola;
+        private System.Windows.Forms.ListBox lbpila;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica2_Laberinto_PilaYCola
+{
+    
+
+    public partial class Form1 : Form
+    {
+        static int nfilas = 11, ncol = 11;
+        int[,] Laberinto = new int[nfilas, ncol];
+
+        int[,] PathCola = new int[nfilas, ncol];
+        int[,] PathPila = new int[nfilas, ncol];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbpila.Items.Clear();
+            lbcola.Items.Clear();
+            Random k;
+            k = new Random();
+            int aleat, valor = 0;
+            Graphics g = pb.CreateGraphics();
+            Pen lapiz = new Pen(Color.DarkBlue);
+            Size s = new System.Drawing.Size(50, 50);
+            Image pasto = Image.FromFile(@"Verde2.bmp");
+            Image pared = Image.FromFile(@"Marron2.bmp");
+            Image entrada = Image.FromFile(@"Azul2.bmp");
+            Image textura;
+            Point p = new Point(0, 0);
+            Rectangle[,] r;
+            r = new Rectangle[nfilas, ncol];
+            for (int i = 0; i < nfilas; i++)
+            {
+                for (int j = 0; j < ncol; j++)
+                {
+                    if ((i == 0 && j == 0) || (i == nfilas - 1 && j == ncol - 1))
+                    {
+                        if (i == 0)
+                        {
+                            textura = entrada;
+                        }
+                        else
+                        {
+                            textura = pasto;
+                        }
+                        valor = 0;
+
+                    }
+                    else
+                    {
+                        if (i % 2 == 0 && j % 2 != 0)
+                        {
+                            textura = pasto;
+                            valor = 0;
+                        }
+                        else
+                        {
+                            aleat = k.Next(2);
+                            if (aleat == 0)
+                            {
+                                textura = pasto;
+                            }
+
+                            else
+                                textura = pared;
+                            valor = aleat;
+                        }
+                    }
+                    Laberinto[i, j] = valor;
+                    PathPila[i, j] = 1;
+                    PathCola[i, j] = 1;
+                    r[i, j].Location = p;
+                    r[i, j].Size = s;
+                    g.DrawImage(textura, r[i, j]);
+                    p.X += 50;
+                }
+
+                p.X = 0;
+                p.Y += 50;
+            }
+        
+
+    }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Queue Cola = new Queue();
+            Queue ColaRecorrido = new Queue();
+            string ColaReserve = "";
+            int filaC, columnaCola;
+
+
+            Cola.Clear();
+            Cola.Enqueue("0,0");
+            bool StopCola = true;
+
+            while (StopCola != false)
+            {
+                if (Cola.Count != 0)
+                {
+                    string[] temp2c = Cola.Peek().ToString().Split(',');
+                    filaC = int.Parse(temp2c[0]);
+                    columnaCola = int.Parse(temp2c[1]);
+                }
+                else
+                {
+                    StopCola = true;
+                    break;
+                }
+
+                PathCola[filaC, columnaCola] = 0;
+
+                if (filaC == 10 && columnaCola == 10)
+                {
+                    StopCola = false;
+                    break;
+                }
+
+                ColaRecorrido = new Queue(Cola);
+
+                while (ColaRecorrido.Count != 0)
+                {
+                    ColaReserve += ColaRecorrido.Dequeue().ToString() + " ; ";
+                }
+
+                lbcola.Items.Add(ColaReserve);
+                ColaReserve = "";
+                Cola.Dequeue();
+
+                if (columnaCola < 10 && PathCola[filaC, columnaCola + 1] == 1 && Laberinto[filaC, columnaCola + 1] == 0)
+                {
+                    Cola.Enqueue(filaC + "," + (columnaCola + 1));
+                    PathCola[filaC, columnaCola + 1] = 0;
+                }
+
+                if (columnaCola != 0 && PathCola[filaC, columnaCola - 1] == 1 && Laberinto[filaC, columnaCola - 1] == 0)
+                {
+                    Cola.Enqueue(filaC + "," + (columnaCola - 1));
+                    PathCola[filaC, columnaCola - 1] = 0;
+                }
+
+                if (filaC < 10 && PathCola[filaC + 1, columnaCola] == 1 && Laberinto[filaC + 1, columnaCola] == 0)
+                {
+                    Cola.Enqueue((filaC + 1) + "," + columnaCola);
+                    PathCola[filaC + 1, columnaCola] = 0;
+                }
+
+                if (filaC != 0 && PathCola[filaC - 1, columnaCola] == 1 && Laberinto[filaC - 1, columnaCola] == 0)
+                {
+                    Cola.Enqueue((filaC - 1) + "," + columnaCola);
+                    PathCola[filaC - 1, columnaCola] = 0;
+                }
+            }
+
+
+            Stack Pila = new Stack();
+            string PilaReserve = "";
+            int filaPila, columnaPila;
+
+            Pila.Clear();
+            Pila.Push("0,0");
+            bool StopPila = true;
+
+            while (StopPila != false)
+            {
+                if (Pila.Count != 0)
+                {
+                    string[] temp = Pila.Peek().ToString().Split(',');
+                    filaPila = int.Parse(temp[0]);
+                    columnaPila = int.Parse(temp[1]);
+                }
+                else
+                {
+                    StopPila = true;
+                    break;
+                }
+
+                PathPila[filaPila, columnaPila] = 0;
+
+                if (filaPila == 10 && columnaPila == 10)
+                {
+                    StopPila = false;
+                    break;
+                }
+
+                Stack PilaRecorrido = (Stack)Pila.Clone();
+
+                while (PilaRecorrido.Count != 0)
+                {
+                    PilaReserve += PilaRecorrido.Pop().ToString() + " ; ";
+                }
+
+                lbpila.Items.Add(PilaReserve);
+                PilaReserve = "";
+                Pila.Pop();
+
+                if (columnaPila < 10 && PathPila[filaPila, columnaPila + 1] == 1 && Laberinto[filaPila, columnaPila + 1] == 0)
+                {
+                    Pila.Push(filaPila + "," + (columnaPila + 1));
+                    PathPila[filaPila, columnaPila + 1] = 0;
+                }
+
+                if (columnaPila != 0 && PathPila[filaPila, columnaPila - 1] == 1 && Laberinto[filaPila, columnaPila - 1] == 0)
+                {
+                    Pila.Push(filaPila + "," + (columnaPila - 1));
+                    PathPila[filaPila, columnaPila - 1] = 0;
+                }
+
+                if (filaPila < 10 && PathPila[filaPila + 1, columnaPila] == 1 && Laberinto[filaPila + 1, columnaPila] == 0)
+                {
+                    Pila.Push((filaPila + 1) + "," + columnaPila);
+                    PathPila[filaPila + 1, columnaPila] = 0;
+                }
+
+                if (filaPila != 0 && PathPila[filaPila - 1, columnaPila] == 1 && Laberinto[filaPila - 1, columnaPila] == 0)
+                {
+                    Pila.Push((filaPila - 1) + "," + columnaPila);
+                    PathPila[filaPila - 1, columnaPila] = 0;
+                }
+            }
+
+
+
+
+            if ((StopCola && StopPila) == false)
+            {
+                MessageBox.Show("Solución encontrada");
+            }
+            else
+            {
+                MessageBox.Show("No hay solución");
+            }
+
+
+            if (Pila.Count == 1)
+            {
+                lbpila.Items.Add(Pila.Pop().ToString());
+            }
+
+            if (Cola.Count == 1)
+            {
+                lbcola.Items.Add(Cola.Dequeue().ToString());
+            }
+
+        }
+    }
+}
